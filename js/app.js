a3ab9a0543cf0b1a40161632f60cd1ef91900dea
Regina Johnson <ReginaBFly@yahoo.com>
2018-05-13 20:38:57 -0400

    feat: Clean up code comments and created move counter

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 //Create a list that holds all of your cards
 let card = document.getElementsByClassName("card");
 let cardsArray = [...card];
+let moves = 0;
 
 //arrays to hold matching and non-matching cards
-let openedCardsArray = []; 
-let matchedCardsArray = []; 
+let openedCardsArray = [];
+let matchedCardsArray = [];
 
 /*
  * Display the cards on the page
@@ -15,7 +16,8 @@ let matchedCardsArray = [];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -29,7 +31,7 @@ function shuffle(array) {
 }
 
 //put shuffled cards on deck
-function putShuffledCardsOnDeck () {
+function putShuffledCardsOnDeck() {
     let shuffledCards = shuffle(cardsArray);
     let deck = document.querySelector(".deck");
     for (let i = 0; i < cardsArray.length; i++) {
@@ -37,37 +39,40 @@ function putShuffledCardsOnDeck () {
     }
     for (let c = 0; c < shuffledCards.length; c++) {
         deck.appendChild(shuffledCards[c]);
-        }
+    }
 };
 
 //request shuffling of the cards when page loads
 window.onload = putShuffledCardsOnDeck();
 
-//display the card's symbol (put this functionality in another function that you call from this one)
-function flipCard (theCardSelected) {
+//display the card's symbol
+function flipCard(theCardSelected) {
     theCardSelected.classList.toggle("open");
     theCardSelected.classList.toggle("show");
 }
 
-function cardEventListener (event) {
+function cardEventListener(event) {
     flipCard(event.target);
+    //add the card to a *list* of "open" cards
     if (event.target.classList.contains('card') && openedCardsArray.length < 2) {
         addToListOpenedCards(event);
     }
     //if the list already has another card, check to see if the two cards match
     if (openedCardsArray.length === 2) {
         if (openedCardsArray[0].children[0].className === openedCardsArray[1].children[0].className) {
+            //if the cards do match, lock the cards in the open position
             trackMatchedCards();
         } else {
             //remove event listener until timer is done
-            for (let i = 0; i < cardsArray.length; i++){
+            for (let i = 0; i < cardsArray.length; i++) {
                 cardsArray[i].removeEventListener("click", cardEventListener);
             };
             //timer slows down flipping of the card
             setTimeout(function flipCardsBackOverAfterDelay() {
+                //if the cards do not match, remove the cards from the list and hide the card's symbol 
                 removeOpenedClasses();
                 //add event listener back after timer expires
-                for (let i = 0; i < cardsArray.length; i++){
+                for (let i = 0; i < cardsArray.length; i++) {
                     cardsArray[i].addEventListener("click", cardEventListener);
                 };
             }, 1000);
@@ -76,24 +81,22 @@ function cardEventListener (event) {
 };
 
 //set up the event listener for a card if it is clicked
-for (let i = 0; i < cardsArray.length; i++){
+for (let i = 0; i < cardsArray.length; i++) {
     cardsArray[i].addEventListener("click", cardEventListener);
+    cardsArray[i].addEventListener("click", increaseMoveCounterFunc);
 };
 
-//add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-function addToListOpenedCards (event) {
+function addToListOpenedCards(event) {
     openedCardsArray.push(event.target);
 }
 
-//if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-function removeOpenedClasses () {
+function removeOpenedClasses() {
     openedCardsArray[0].classList.remove("open", "show");
     openedCardsArray[1].classList.remove("open", "show");
     openedCardsArray = [];
 }
 
-//if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-function trackMatchedCards () {
+function trackMatchedCards() {
     openedCardsArray[0].classList.add("match", "disabled");
     openedCardsArray[1].classList.add("match", "disabled");
     openedCardsArray[0].classList.remove("show", "open");
@@ -103,6 +106,10 @@ function trackMatchedCards () {
     openedCardsArray = [];
 }
 
+//increment the move counter and display it on the page
+function increaseMoveCounterFunc () {
+    moves++;
+    document.querySelector("span").textContent = moves;
+}
 
-//increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 //if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
