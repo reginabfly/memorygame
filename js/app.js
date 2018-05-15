7be2388d25f2c2e8176519485fb23bb6b806d608
Regina Johnson <ReginaBFly@yahoo.com>
2018-05-15 09:12:30 -0400

    feat: Add timer to memory game

--- js/app.js
+++ js/app.js
@@ -117,6 +117,11 @@ function trackMatchedCards() {
 function increaseMoveCounterFunc () {
     moves++;
     document.querySelector("span").textContent = moves;
+    if(moves == 1){
+        second = 0;
+        minute = 0; 
+        startTimer();
+    }
 }
 
 //remove stars as move counter increases
@@ -146,12 +151,31 @@ function resetGame() {
     //set arrays back to empty
     matchedCardsArray = [];
     openedCardsArray = [];
+    //reset timer
+    second = 0;
+    minute = 0; 
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
     //turn over all cards
-    cardsArray.classList.remove("open", "show");
+    // cardsArray.classList.remove("open", "show");
 }
 
 //timer
-
+var second = 0, minute = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+" mins "+second+" secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+    },1000);
+}
 
 //if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 function finalScoreModal () {
